f7cee0453fbbb7fe28338e47ab59f56af50455d4
Emmanuel <emmanuelkibe148@gmail.com>
2026-01-01 16:07:16 +0300

    Add Mutation class to schema for enhanced GraphQL functionality

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,8 +1,11 @@
 import graphene
-from crm.schema import CRMQuery  # Import the query from your app
+from crm.schema import CRMQuery, Mutation as CRMutation  # Import the query and mutation from your app
 
 # We inherit from CRMQuery and graphene.ObjectType to combine them
 class Query(CRMQuery, graphene.ObjectType):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(CRMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
